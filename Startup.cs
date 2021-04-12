02b3bc85d91a1e2587b2ff3467ba16a3ee8c0a1f
Joseph Venuto <jbvenuto@gmail.com>
2021-04-12 11:27:06 -0400

    configured db for production

--- Startup.cs
+++ Startup.cs
@@ -15,12 +15,14 @@ namespace RecipeBoxMVC
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -28,7 +30,19 @@ namespace RecipeBoxMVC
             services.AddControllersWithViews();
 
             services.AddDbContext<RecipeBoxContext>(options =>
-                options.UseSqlite(Configuration.GetConnectionString("RecipeBoxContext")));
+        {
+            var connectionString = Configuration.GetConnectionString("RecipeBoxContext");
+
+            if (Environment.IsDevelopment())
+            {
+                options.UseSqlite(connectionString);
+            }
+            else
+            {
+                options.UseSqlServer(connectionString);
+            }
+        });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
