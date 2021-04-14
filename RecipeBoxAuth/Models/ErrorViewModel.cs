7d42c46e97c4495c4eb8d87d9debebcde3412190
Joseph Venuto <jbvenuto@gmail.com>
2021-04-13 20:13:25 -0400

    added authentication

---
+++ RecipeBoxAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RecipeBoxAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
